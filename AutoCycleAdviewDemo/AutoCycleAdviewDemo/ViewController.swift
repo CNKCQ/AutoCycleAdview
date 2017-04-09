c235c85d78fd4760e6c9ac2197260bc9670000db
KingCQ <wangchengqvan@hotmail.com>
2017-04-09 18:24:27 +0800

    format

--- AutoCycleAdviewDemo/AutoCycleAdviewDemo/ViewController.swift
+++ AutoCycleAdviewDemo/AutoCycleAdviewDemo/ViewController.swift
@@ -4,7 +4,6 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
     var showImage: UIImageView?
 
@@ -16,7 +15,8 @@ class ViewController: UIViewController {
             "http://ww1.sinaimg.cn/mw690/692a6bbcgw1f4fz7s830fj20gg0o00y5.jpg",
             "http://ww1.sinaimg.cn/mw690/692a6bbcgw1f4fz6g6wppj20ms0xp13n.jpg",
             "http://ww3.sinaimg.cn/mw690/81309c56jw1f4sx4ybttdj20ku0vd0ym.jpg",
-            "http://ww4.sinaimg.cn/mw690/9844520fjw1f4fqribdg1j21911w0kjn.jpg"]
+            "http://ww4.sinaimg.cn/mw690/9844520fjw1f4fqribdg1j21911w0kjn.jpg",
+        ]
         let adDescs = ["I was angry friende", "Itold my wrath,my wrath did end.", "I was angry with my foe:", "Itold it not,my wrath did grow ", "And I watered it in fears"]
         let ad = AutoCycleAdview(frame: CGRect(x: 0, y: 100 + 10, width: view.frame.width, height: 240))
         ad.pageControlAlignment = .right
